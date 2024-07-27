85117cff9c77cc4daa407e7ce77ae89421971005
David Kawadkar <davidkawadkar007@gmail.com>
2024-07-27 11:46:16 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className="bg-dark text-center text-light py-2">
+    <div className="bg-dark text-center text-light py-3">
       <p className="text-center">
       Copyright &copy; TodosList.com
       </p>
-    </footer>
+    </div>
   );
 }
 
